d324435acf0561ed708bc7abbdcee9b47316c50f
worldrecord <martinleonardi88@yahoo.com>
2016-10-29 11:01:53 +0700

    commit pertama

---
+++ Latihan1_1/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace Latihan1_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.tabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.ImageList = this.imageList1;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(272, 294);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.ImageIndex = 2;
+            this.tabPage1.Location = new System.Drawing.Point(4, 23);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.tabPage1.Size = new System.Drawing.Size(264, 267);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Open";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.ImageIndex = 3;
+            this.tabPage2.Location = new System.Drawing.Point(4, 23);
+            this.tabPage2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.tabPage2.Size = new System.Drawing.Size(264, 267);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Play";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "1.png");
+            this.imageList1.Images.SetKeyName(1, "2.png");
+            this.imageList1.Images.SetKeyName(2, "Render_Luffy_Afro_by_Kakarot.png");
+            this.imageList1.Images.SetKeyName(3, "aNe2Rxr_460s_v1.jpeg");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 308);
+            this.Controls.Add(this.tabControl1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Latihan1_1";
+            this.tabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.ImageList imageList1;
+    }
+}
+
---
+++ Latihan1_2/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace Latihan_1_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.vScrollBar1 = new System.Windows.Forms.VScrollBar();
+            this.vScrollBar2 = new System.Windows.Forms.VScrollBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.test = new System.Windows.Forms.Label();
+            this.min = new System.Windows.Forms.Label();
+            this.max = new System.Windows.Forms.Label();
+            this.calender = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // vScrollBar1
+            // 
+            this.vScrollBar1.Cursor = System.Windows.Forms.Cursors.NoMoveVert;
+            this.vScrollBar1.Location = new System.Drawing.Point(9, 35);
+            this.vScrollBar1.Name = "vScrollBar1";
+            this.vScrollBar1.Size = new System.Drawing.Size(48, 372);
+            this.vScrollBar1.TabIndex = 2;
+            this.vScrollBar1.Scroll += new System.Windows.Forms.ScrollEventHandler(this.vScrollBar1_Scroll);
+            // 
+            // vScrollBar2
+            // 
+            this.vScrollBar2.Cursor = System.Windows.Forms.Cursors.NoMoveVert;
+            this.vScrollBar2.Location = new System.Drawing.Point(373, 35);
+            this.vScrollBar2.Name = "vScrollBar2";
+            this.vScrollBar2.Size = new System.Drawing.Size(50, 378);
+            this.vScrollBar2.TabIndex = 3;
+            this.vScrollBar2.Scroll += new System.Windows.Forms.ScrollEventHandler(this.vScrollBar2_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Perpetua", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Lime;
+            this.label1.Location = new System.Drawing.Point(85, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 42);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "MIN";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Perpetua", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(255, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 42);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "MAX";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // test
+            // 
+            this.test.AutoSize = true;
+            this.test.Font = new System.Drawing.Font("Elephant", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.test.ForeColor = System.Drawing.Color.Black;
+            this.test.Location = new System.Drawing.Point(188, 148);
+            this.test.Name = "test";
+            this.test.Size = new System.Drawing.Size(29, 41);
+            this.test.TabIndex = 6;
+            this.test.Text = "-";
+            this.test.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // min
+            // 
+            this.min.AutoSize = true;
+            this.min.Font = new System.Drawing.Font("Perpetua", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.min.ForeColor = System.Drawing.Color.Silver;
+            this.min.Location = new System.Drawing.Point(165, 247);
+            this.min.Name = "min";
+            this.min.Size = new System.Drawing.Size(26, 33);
+            this.min.TabIndex = 7;
+            this.min.Text = "-";
+            // 
+            // max
+            // 
+            this.max.AutoSize = true;
+            this.max.Font = new System.Drawing.Font("Mistral", 21.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.max.ForeColor = System.Drawing.Color.Silver;
+            this.max.Location = new System.Drawing.Point(369, 247);
+            this.max.Name = "max";
+            this.max.Size = new System.Drawing.Size(22, 34);
+            this.max.TabIndex = 8;
+            this.max.Text = "-";
+            // 
+            // calender
+            // 
+            this.calender.CalendarForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.calender.CalendarMonthBackground = System.Drawing.SystemColors.Highlight;
+            this.calender.CalendarTitleBackColor = System.Drawing.SystemColors.HighlightText;
+            this.calender.CalendarTitleForeColor = System.Drawing.SystemColors.MenuText;
+            this.calender.Location = new System.Drawing.Point(0, 12);
+            this.calender.Name = "calender";
+            this.calender.Size = new System.Drawing.Size(429, 20);
+            this.calender.TabIndex = 9;
+            this.calender.ValueChanged += new System.EventHandler(this.calender_ValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(432, 482);
+            this.Controls.Add(this.calender);
+            this.Controls.Add(this.max);
+            this.Controls.Add(this.min);
+            this.Controls.Add(this.test);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.vScrollBar2);
+            this.Controls.Add(this.vScrollBar1);
+            this.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.Name = "Form1";
+            this.Text = "Latihan1_2";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.VScrollBar vScrollBar1;
+        private System.Windows.Forms.VScrollBar vScrollBar2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label test;
+        private System.Windows.Forms.Label min;
+        private System.Windows.Forms.Label max;
+        private System.Windows.Forms.DateTimePicker calender;
+    }
+}
+
